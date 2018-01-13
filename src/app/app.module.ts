0895161ff2e24a71a2575fd9f523d4dda634a3b2
Israel Omede <is428el@gmail.com>
2018-01-13 14:44:20 -0500

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule, ErrorHandler } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
-
+import { Camera } from '@ionic-native/camera'; 
 import { AboutPage } from '../pages/about/about';
 import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
@@ -33,7 +33,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   ],
   providers: [
     StatusBar,
-    SplashScreen,
+    SplashScreen, Camera,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,61 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
-
+import { NavController, AlertController } from 'ionic-angular';
+import { Camera, CameraOptions } from '@ionic-native/camera';
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
-  constructor(public navCtrl: NavController) {
+  public photos: any;
+  public base64Image: string;
+  constructor(public navCtrl: NavController, private camera: Camera, private alertCtrl: AlertController) {
 
   }
+  ngOnInit(){
+    this.photos = [];
+  }
 
+  takePhoto(){
+    const options: CameraOptions = {
+      quality: 50,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE
+    }
+    
+    this.camera.getPicture(options).then((imageData) => {
+     // imageData is either a base64 encoded string or a file URI
+     // If it's base64:
+     this.base64Image = 'data:image/jpeg;base64,' + imageData;
+     this.photos.push(this.base64Image);
+     this.photos.reverse();
+    }, (err) => {
+     // Handle error
+    });
+  }
+
+  deletePhoto(index){
+    let alert = this.alertCtrl.create({
+      title: 'Confirm Delete',
+      message: 'Do you want to delete this image?',
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          handler: () => {
+            console.log('Cancelled');
+          }
+        },
+        {
+          text: 'Delete',
+          handler: () => {
+            this.photos.splice(index, 1);
+          }
+        }
+      ]
+    });
+    alert.present();
+    // this.photos.splice(index, 1);
+
+  }
 }
